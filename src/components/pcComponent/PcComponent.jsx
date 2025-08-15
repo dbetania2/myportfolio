337ef6eaee06f23222a6e149956de41dfc8aaa21
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2025-08-15 11:06:37 -0300

    refactor: Ajustes en los componentes de la pantalla del PC
    Se eliminó la imagen de fondo de los componentes de la pantalla del PC y se realizaron ajustes menores para mejorar la presentación visual. Esto forma parte de la reestructuración general del portfolio.

--- src/components/pcComponent/PcComponent.jsx
+++ src/components/pcComponent/PcComponent.jsx
@@ -1,44 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './PcComponent.css';
 import PcScreen from '../PcScreen/PcScreen.jsx';
 
-const PcComponent = () => {
-  const [showScreen, setShowScreen] = useState(false);
-
-  const handlePcClick = () => {
-    setShowScreen(true);
-  };
-
-  const handleCloseScreen = () => {
-    setShowScreen(false);
-  };
-
-  return (
-    <>
-      <img 
-        src="/src/objetos-img/pc.png" 
-        alt="pc" 
-        className="map-object" 
-        id="pc" 
-        onClick={handlePcClick} 
-        
-      />
-      {showScreen && (
-        <div className={`screen-overlay ${showScreen ? 'show' : ''}`} onClick={handleCloseScreen}>
-          <div className="screen-content" onClick={(e) => e.stopPropagation()}>
-            <PcScreen /> 
-            <button className="close-button" onClick={handleCloseScreen}>Cerrar</button>
-          </div>
+/**
+ * Componente para mostrar la PC con pantalla y cerrar.
+ * @param {{onClose: () => void, onNavigate: (section: string) => void}} props
+ */
+const PcComponent = ({ onClose, onNavigate }) => {
+    return (
+        <div className="screen-overlay show" onClick={onClose}>
+            <div className="screen-content" onClick={e => e.stopPropagation()}>
+                {/* Pasamos onNavigate a PcScreen */}
+                <PcScreen onNavigate={onNavigate} /> 
+                <button className="close-button" onClick={onClose}>Cerrar</button>
+            </div>
         </div>
-      )}
-    </>
-  );
+    );
 };
 
 export default PcComponent;
-
-
-
-
-
-
